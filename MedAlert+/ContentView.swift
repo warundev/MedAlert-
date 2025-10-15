e1066a7884fa58f09393272c2625a38f951319d4
Waruna Liyanapathirana <2020icts03@vau.jfn.ac.lk>
2025-10-15 12:33:47 +0530

    Initial Commit

---
+++ MedAlert+/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MedAlert+
+//
+//  Created by Waruna Liyanapathirana on 2025-10-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MedAlert+/MedAlert_App.swift
@@ -0,0 +1,17 @@
+//
+//  MedAlert_App.swift
+//  MedAlert+
+//
+//  Created by Waruna Liyanapathirana on 2025-10-15.
+//
+
+import SwiftUI
+
+@main
+struct MedAlert_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
